b1fa8c33a4657a11e6081f03ad2f2c0b03b4d7c6
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-01-28 18:28:53 -0300

    Add Project class

---
+++ src/objects/project.js
@@ -0,0 +1,56 @@
+import DataController from "../controllers/data-controller";
+
+export default class Project {
+  constructor(title, description, dueDate, tasks) {
+    this.id = DataController.generateId();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.tasks = tasks;
+    this.isComplete = false;
+  }
+
+  addTask(task) {
+    this.tasks.push(task);
+  }
+
+  removeTask(task) {
+    this.tasks = this.tasks.filter(t => t !== task);
+  }
+
+  // This method is used to update a task after it has been edited
+  updateTask(task, newTask) {
+    const index = this.tasks.indexOf(task);
+    this.tasks[index] = newTask;
+  }
+
+  updateTitle(title) {
+    this.title = title;
+  }
+
+  updateDescription(description) {
+    this.description = description;
+  }
+
+  updateDueDate(dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  toggleComplete() {
+    this.isComplete = !this.isComplete;
+  }
+
+  getProgress() {
+    const totalTasks = this.tasks.length;
+    const completedTasks = this.tasks.filter(task => task.isComplete).length;
+    return (completedTasks / 100) * totalTasks;
+  }
+
+  getCompletedTasks() {
+    return this.tasks.filter(task => task.isComplete);
+  }
+  
+  getPendingTasks() {
+    return this.tasks.filter(task => !task.isComplete);
+  }
+}
