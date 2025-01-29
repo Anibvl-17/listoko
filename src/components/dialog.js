ba602095a7c0c7e4ef4dbb2d146caaa7d84e0cea
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-01-29 20:41:43 -0300

    Rebuild dialog component into class, without event listeners yet

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -1,40 +1,65 @@
 import Project from "../objects/project";
 import DataController from "../controllers/data-controller";
 
-import { loadSidebarProjects, loadLastProject } from "../controllers/sidebar-controller";
+//import { loadSidebarProjects, loadLastProject } from "../controllers/sidebar-controller";
 
-const newProjectItem = document.getElementById('new-project');
-const newProjectDialog = document.getElementById('new-project-dialog');
-const newProjectButton = document.getElementById('add-project');
-const newProjectForm = document.forms.newProject;
+export const DIALOG_EDIT = 'dialog_edit';
+export const DIALOG_NEW = 'dialog_new';
+export const DIALOG_TASK = 'dialog_task';
+export const DIALOG_PROJECT = 'dialog_project';
 
-// Handles the click event on the 'New Project' list item
-newProjectItem.addEventListener('click', () => {
-  newProjectDialog.showModal();
-});
+export class Dialog {
+  static dialog = document.getElementById('dialog');
+  static dialogTitle = document.getElementById('dialog-title');
+  static dialogConfirm = document.getElementById('dialog-confirm');
+  static dialogCancel = document.querySelector('.dialog-cancel');
+  static dialogForm = document.forms.dialogForm;
 
-// Handles the click event on the 'Add Project' button
-newProjectButton.addEventListener('click', (e) => {
-  // Prevent the form from submitting
-  e.preventDefault();
-  
-  // Get the form data
-  const newProjectData = new FormData(newProjectForm);
-  
-  // Create a new project object
-  const project = new Project(
-    newProjectData.get('project-name'),
-    newProjectData.get('project-description'),
-    newProjectData.get('project-due-date'),
-    [] // Tasks array
-  );
-  
-  // Save the new project and reload the sidebar projects list
-  DataController.saveProject(project);
-  loadSidebarProjects();
+  static inputName = document.getElementById('form-name');
+  static inputDescription = document.getElementById('form-description');
+  static inputDueDate = document.getElementById('form-due-date');
 
-  // Load the last project, which is the new project
-  loadLastProject();
+  // The dialog constructor receives the mode (new or edit), the object (project or task), and
+  // data only if the mode is edit. The data is the object to be edited.
+  constructor(mode, object, data = null) {
+    this.mode = mode;
+    this.object = object;
+    this.data = data;
 
-  newProjectDialog.close();
-});
+    Dialog.dialog.addEventListener('close', () => Dialog.dialogForm.reset() );
+    Dialog.dialogCancel.addEventListener('click', () => Dialog.dialog.close() );
+
+    if (mode === DIALOG_EDIT) {
+      Dialog.dialogConfirm.addEventListener('click', this.confirmEdit);
+      Dialog.dialogTitle.textContent = object === DIALOG_PROJECT ? 'Edit Project' : 'Edit Task';
+      Dialog.dialogConfirm.textContent = object === DIALOG_PROJECT ? 'Save Project' : 'Save Task';
+
+      Dialog.inputName.value = data.title;
+      Dialog.inputDescription.value = data.description;
+      Dialog.inputDueDate.value = data.dueDate.toString();
+    } else {
+      Dialog.dialogConfirm.addEventListener('click', this.confirmNew);
+      Dialog.dialogTitle.textContent = object === DIALOG_PROJECT ? 'New Project' : 'New Task';
+      Dialog.dialogConfirm.textContent = object === DIALOG_PROJECT ? 'Create Project' : 'Create Task';
+    }
+  }
+
+  show() {
+    Dialog.dialog.showModal();
+  }
+
+  confirmEdit(e) {
+    // Prevent the form from submitting
+    e.preventDefault();
+    
+    alert('Edit confirmed, new name ' + Dialog.inputName.value);
+    
+  }
+
+  confirmNew(e) {
+    // Prevent the form from submitting
+    e.preventDefault();
+    
+    alert('New confirmed, name ' + Dialog.inputName.value);
+  }
+}
