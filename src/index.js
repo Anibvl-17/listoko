f2fc0f41b5299cec650f63c9a656e959e4f9ae18
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-02-01 16:53:53 -0300

    Add checkStorage function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import './styles.css';
 import './components/dialog';
 
 import { loadSidebar } from './controllers/sidebar-controller.js';
+import { DataController } from './controllers/data-controller.js';
 
+DataController.checkStorage();
 loadSidebar();
