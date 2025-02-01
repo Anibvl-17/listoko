137e929a6bbe3160c57ef321bca78fdd1155c5b3
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-02-01 16:13:49 -0300

    Refactor Project class to use Task class in tasks

--- src/objects/project.js
+++ src/objects/project.js
@@ -1,9 +1,14 @@
+import { Task } from "./task";
+
 export default class Project {
   constructor(name, description, dueDate, tasks) {
     this.name = name;
     this.description = description;
     this.dueDate = dueDate;
-    this.tasks = tasks;
+    this.tasks = [];
+    tasks.forEach((task, index) => {
+      this.tasks[index] = new Task(task.name, task.description, task.dueDate, task.isComplete);
+    });
     this.isComplete = false;
   }
 
