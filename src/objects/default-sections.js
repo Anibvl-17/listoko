257a3de3552a6484a68734487826473d36fb6a76
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-02-04 23:49:25 -0300

    Improve default sections description and tasks

--- src/objects/default-sections.js
+++ src/objects/default-sections.js
@@ -13,25 +13,23 @@ const todayDate = new Date().toISOString().substring(0, 10);
 const todaySection = new Project(
   'Today',
   'Stay focused on what matters now! Track your tasks for the day and complete them before midnight. ' +
-    'A fresh start awaits tomorrow—make every task count!',
+    'A fresh start awaits tomorrow!',
   new Date().toDateString(),
   [
-    new Task('Task 1', 'Description 1', todayDate, true),
-    new Task('Task 2', 'Description 2', todayDate, false),
-    new Task('Task 3', 'Description 3', todayDate, true)
+    new Task('Clean my bedroom', 'This is a default task, you can edit it, but cleaning your bedroom sounds like a good idea... :)', todayDate, false),
+    new Task('Do exercise', 'Exercising is good for health and improves your mood, give it a try!', todayDate, false),
+    new Task('Read 20 minutes', 'Not scrolling through social media! Read about something you are interested, a book, comic, anything!', todayDate, false)
   ]
 );
-
-
 
 const quicklistSection = new Project(
   'Quicklist',
   'Jot down quick tasks in a snap! This list is your instant workspace for ideas and ' +
-    'reminders. It stays until you reset it—perfect for fast notes and urgent to-dos!',
+    'reminders. It stays until you reset it, perfect for fast notes and urgent to-dos!',
   new Date().toDateString(),
   [
-    new Task('Task 1', 'Description 1', todayDate, false),
-    new Task('Task 2', 'Description 2', todayDate, true),
-    new Task('Task 3', 'Description 3', todayDate, false)
+    new Task('Buy: Umbrella', 'This is a default task. You can delete or edit this!', todayDate, false),
+    new Task('Try The Odin Project', 'Give it a try, it\'s an awesome resource to learn about web development if you don\'t know where to start!', todayDate, true),
+    new Task('Watch: Interestellar', 'People say it\'s an awesome movie, people also say it isn\'t really awesome. You decide.', todayDate, false)
   ]
 );
